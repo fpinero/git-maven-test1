4ef43be7bb977b75eed913b80b82c4c4397a2cdc
Fernando Piñero <Fernando Piero>
2017-05-29 13:41:44 +0200

    primer commit

---
+++ src/main/java/com/fpe/git_maven_test1/App.java
@@ -0,0 +1,13 @@
+package com.fpe.git_maven_test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello again World!" );
+    }
+}
